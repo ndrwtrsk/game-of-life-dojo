4ee11560b3c8ed0a91e60cf7d344900391a81bde
ndrwtrsk <andrew.torski@gmail.com>
2017-10-30 22:35:52 +0100

    init

---
+++ src/main/java/nd/rw/gameoflifedojo/GameOfLifeDojoApplication.java
@@ -0,0 +1,12 @@
+package nd.rw.gameoflifedojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameOfLifeDojoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GameOfLifeDojoApplication.class, args);
+  }
+}
---
+++ src/test/java/nd/rw/gameoflifedojo/GameOfLifeDojoApplicationTests.java
@@ -0,0 +1,16 @@
+package nd.rw.gameoflifedojo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GameOfLifeDojoApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
